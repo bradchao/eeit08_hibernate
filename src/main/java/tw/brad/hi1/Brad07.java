043e2cfb8f64e0a76ef7de7a2a05525ee6e7c4d6
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-22 11:06:56 +0800

    07221106

--- src/main/java/tw/brad/hi1/Brad07.java
+++ src/main/java/tw/brad/hi1/Brad07.java
@@ -10,18 +10,40 @@ public class Brad07 {
 	public static void main(String[] args) {
 		MemberDao dao = new MemberDao();
 		
-		Member member = new Member();
-		member.setAccount("ok2");
-		member.setPasswd(BCrypt.hashpw("123456", BCrypt.gensalt()));
-		member.setName("OK2");
+//		Member member = new Member();
+//		member.setAccount("ok3");
+//		member.setPasswd(BCrypt.hashpw("123456", BCrypt.gensalt()));
+//		member.setName("OK3");
+//		
+//		MemberInfo info = new MemberInfo();
+//		info.setBirthday("1999-03-04");
+//		info.setGender("female");
+//		
+//		member.setMemberinfo(info);
+//		
+//		dao.save(member);
+
+		//----------------
+//		Member member = dao.getById(1);
+//		
+//		MemberInfo info = new MemberInfo();
+//		info.setBirthday("1999-05-06");
+//		info.setGender("male");
+//		
+//		member.setMemberinfo(info);
+//		
+//		dao.update(member);
 		
-		MemberInfo info = new MemberInfo();
-		info.setBirthday("1999-01-02");
-		info.setGender("male");
+		//------------------
+//		Member member = dao.getById(15);
+//		dao.delete(member);
 		
-		member.setMemberinfo(info);
+		Member member = dao.getById(1);
+		MemberInfo info =  member.getMemberinfo();
+		System.out.println(info.getBirthday());
 		
-		dao.save(member);
+		
+	
 	}
 
 }
